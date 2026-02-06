977b4202968f27376850f650e61adecbbe2c4b65
Nate Mitelman <mitelman@umich.edu>
2026-02-06 13:29:08 -0500

    created the __eq__

--- Discussion3.py
+++ Discussion3.py
@@ -26,6 +26,10 @@ class Rectangle():
     # Returns a boolean value
 
     # YOUR CODE HERE
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        return self.width == other.width and self.height == other.height
 
 
     
